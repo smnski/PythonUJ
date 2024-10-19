e5de6131aa1c3fed12a7dc8f1d41718dc4b787b2
avren <szymonurbanski217@gmail.com>
2024-10-19 23:12:34 +0200

    Brakujaca czesc zadania

--- cw2/zad_2_12.py
+++ cw2/zad_2_12.py
@@ -1,5 +1,8 @@
 line = "raz dwa \n trzy cztery \t piec szesc"
 words = line.split()
-result = "".join(word[0] for word in words)
 
-print(result)
+result_first = "".join(word[0] for word in words)
+result_last = "".join(word[len(word)-1] for word in words)
+
+print("Pierwsze znaki: " + result_first)
+print("Ostatnie znaki: " + result_last)
